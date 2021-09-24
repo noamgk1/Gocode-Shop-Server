0cd65e4d2117c3b191ce512aa230c7c7fdf09eac
noamgk1 <noamgk1@gmail.com>
2021-09-24 13:35:06 +0300

    check

--- client/src/views/NavBar.js
+++ client/src/views/NavBar.js
@@ -23,7 +23,50 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import LoginIcon from "@mui/icons-material/Login";
 import { useHistory } from "react-router-dom";
 
+import Link from "@material-ui/core/Link";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import { withStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: 270,
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function NavBar() {
+  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -57,6 +100,22 @@ function NavBar() {
 
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
   };
 
   const menuId = "primary-search-account-menu";
@@ -98,51 +157,110 @@ function NavBar() {
   );
 
   const mobileMenuId = "primary-search-account-menu-mobile";
+  // const renderMobileMenu = (
+  //   <Paper sx={{ width: 320 }}>
+  //     <MenuList dense>
+  //       <Menu
+  //         anchorEl={mobileMoreAnchorEl}
+  //         anchorOrigin={{
+  //           vertical: "top",
+  //           horizontal: "right",
+  //         }}
+  //         id={mobileMenuId}
+  //         keepMounted
+  //         transformOrigin={{
+  //           vertical: "top",
+  //           horizontal: "right",
+  //         }}
+  //         open={isMobileMenuOpen}
+  //         onClose={handleMobileMenuClose}
+  //       >
+  //         <MenuItem>
+  //           {!user.user && <SignInSide />}
+  //           {user.user && <Logout />}
+  //         </MenuItem>
+  //         <Divider />
+  //         <MenuItem onClick={handleProfileMenuOpen}>
+  //           <IconButton
+  //             size="large"
+  //             aria-label="account of current user"
+  //             aria-controls="primary-search-account-menu"
+  //             aria-haspopup="true"
+  //             color="inherit"
+  //           >
+  //             <MenuIcon />
+  //           </IconButton>
+  //           <h6>More</h6>
+  //         </MenuItem>
+  //       </Menu>
+  //     </MenuList>
+  //   </Paper>
+  // );
+
   const renderMobileMenu = (
-    <Paper sx={{ width: 320 }}>
-      <MenuList dense>
-        <Menu
-          anchorEl={mobileMoreAnchorEl}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          id={mobileMenuId}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={isMobileMenuOpen}
-          onClose={handleMobileMenuClose}
-        >
-          <MenuItem>
+    <Grid container component="main" className={classes.root}>
+      <Grid component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <PersonPinIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome {user.user}
+          </Typography>
+
+          <MenuList>
+            <MenuItem onClick={controlPage}>
+              <ListItemIcon>
+                <SettingsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText inset>Control</ListItemText>
+            </MenuItem>
+            <Divider />
+
+            <MenuItem onClick={loginPage}>
+              <LoginIcon fontSize="small" />
+
+              <ListItemText inset>Login</ListItemText>
+            </MenuItem>
+          </MenuList>
+
+          {/* <MenuItem>
             {!user.user && <SignInSide />}
             {user.user && <Logout />}
-          </MenuItem>
+          </MenuItem> */}
           <Divider />
-          <MenuItem onClick={handleProfileMenuOpen}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="primary-search-account-menu"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <h6>More</h6>
-          </MenuItem>
-        </Menu>
-      </MenuList>
-    </Paper>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signUp" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+          <Box mt={5}></Box>
+        </div>
+      </Grid>
+    </Grid>
   );
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar color="#5F9EA0">
-        <Toolbar>
-          {/* <IconButton
+    <React.Fragment key={"right"}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar color="#5F9EA0">
+          <Toolbar>
+            {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -152,46 +270,55 @@ function NavBar() {
             <MenuIcon />
           </IconButton> */}
 
-          <Typography variant="h6" noWrap component="div">
-            <NavLink to="/">My Shop</NavLink>
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <SideCart />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            {!user.user && <SignInSide />}
-            {user.user && <Logout />}
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-      <br />
-      <br />
-      <br />
-      <br />
-    </Box>
+            <Typography variant="h6" noWrap component="div">
+              <NavLink to="/">My Shop</NavLink>
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <SideCart />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              {!user.user && <SignInSide />}
+              {user.user && <Logout />}
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen && toggleDrawer("right", true)}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <SwipeableDrawer
+          anchor={"right"}
+          open={state["right"]}
+          onClose={toggleDrawer("right", false)}
+          onOpen={toggleDrawer("right", true)}
+        >
+          {renderMobileMenu}
+        </SwipeableDrawer>
+
+        {renderMenu}
+        <br />
+        <br />
+        <br />
+        <br />
+      </Box>
+    </React.Fragment>
   );
 }
 
